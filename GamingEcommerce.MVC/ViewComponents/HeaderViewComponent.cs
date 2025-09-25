68c3bf6dfd1b4a6b273839ac5ef75c567a5f0da5
haci1994 <mmzadj@gmail.com>
2025-09-25 17:56:45 +0400

    added Addtocart to popular products

--- GamingEcommerce.MVC/ViewComponents/HeaderViewComponent.cs
+++ GamingEcommerce.MVC/ViewComponents/HeaderViewComponent.cs
@@ -1,11 +1,6 @@
-﻿using Azure;
-using GamingEcommerce.BLL.Services.GeneralServices;
-using GamingEcommerce.BLL.Services.WebsiteServices;
+﻿using GamingEcommerce.BLL.Services.WebsiteServices;
 using GamingEcommerce.BLL.ViewModels.WebsiteViewModels;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using System.Text.Json.Serialization;
 
 namespace GamingEcommerce.MVC.ViewComponents
 {
